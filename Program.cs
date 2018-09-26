084f5cd9c4f4d9d45d13270ee11461e2d7912481
Phillip Clontz <phillipac718@gmail.com>
2018-09-26 23:58:52 +0000

    Added the basic functions to be filled out

--- Program.cs
+++ Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+﻿using SecretSanta.Models;
 
-namespace workspace
+namespace SecretSanta
 {
     class Program
     {
+        public const string _familyPath = "/data/family.json";
+        public const int _secretAmount = 2;
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Gathering gathering = new Gathering(_familyPath);
+            bool success = false;
+            if(!gathering.Validate())
+            {
+                Console.WriteLine("Make sure every family member is included in the Secret Santa.");
+            }
+            else
+            {
+                success = gathering.Shuffle(_secretAmount);
+                if(success)
+                {
+                    gathering.CreateCsv();
+                    Console.WriteLine("Secret Santa list was successfully created");
+                }
+                else
+                {
+                    Console.WriteLine("The amount specified are higher or equal to the number of players.");
+                }
+            }
+            Console.WriteLine("Press any button to close.");
+            Console.ReadKey();
         }
     }
 }
--- models/Gathering.cs
+++ models/Gathering.cs
@@ -6,16 +6,43 @@ namespace SecretSanta.Models
     
     public class Gathering : List<Person>
     {
-        ///<Summary>
+        private string _infoPath;
+        
+        public Gathering(string infoPath){
+            _infoPath = infoPath;
+        }
+        ///<summary>
         ///Validates if the supplied gather is valid
         ///all family members of a user should be members
         ///of the gather.
-        ///</Summary>
+        ///</summary>
         public bool Validate()
         {
-            return true;
-            
+            return true;  
         } 
+        
+        ///<summary>
+        ///Assignes everyone people for secret santa. 
+        ///<param ref="numberOfPlayers">The number of people to assign
+        ///should be less than the number of people in the group</param>
+        ///</summary>
+        public bool Shuffle(int numberOfPlayers = 1)
+        {
+            if(numberOfPlayers >= this.Count){
+                return false;
+            }
+            
+            return true;
+        }
+        
+        ///<summary>
+        ///Creates CSV file to display information about the group
+        ///includes who should be assigned to whom
+        ///</summary>
+        public void CreateCsv()
+        {
+            
+        }
     }
     
 }
